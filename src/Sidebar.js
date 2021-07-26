c91c199b4bab141ea8e5d6236614bd30abe77e48
pragna88 <patelpragna655@gmail.com>
2021-07-26 13:31:26 -0500

    changed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -91,64 +91,24 @@ const Sidebar = ({activeSidebar,setActiveSidebar,setWeightsDone, setData}) =>{
 													<ToggleButton
 														type="radio"
 														variant="outline-secondary"
-														name="weight"
-														value="low"
-														checked={measure.weight === 'low'}
-														onChange={(e) =>
-															handleChange(
-																e.currentTarget.value,
-																e.currentTarget.name,
-																measure.value,
-																'hab'
-															)}
 													>
 														Low ROI [0]
 													</ToggleButton>
 													<ToggleButton
 														type="radio"
 														variant="outline-secondary"
-														name="weight"
-														value="medium"
-														checked={measure.weight === 'medium'}
-														onChange={(e) =>
-															handleChange(
-																e.currentTarget.value,
-																e.currentTarget.name,
-																measure.value,
-																'hab'
-															)}
 													>
 														Restore [1]
 													</ToggleButton>
 													<ToggleButton
 														type="radio"
 														variant="outline-secondary"
-														name="weight"
-														value="high"
-														checked={measure.weight === 'high'}
-														onChange={(e) =>
-															handleChange(
-																e.currentTarget.value,
-																e.currentTarget.name,
-																measure.value,
-																'hab'
-															)}
 													>
 														Enhance [2]
 													</ToggleButton>
 													<ToggleButton
 														type="radio"
 														variant="outline-secondary"
-														name="weight"
-														value="high"
-														checked={measure.weight === 'high'}
-														onChange={(e) =>
-															handleChange(
-																e.currentTarget.value,
-																e.currentTarget.name,
-																measure.value,
-																'hab'
-															)}
 													>
 														Maintain [3]
 													</ToggleButton>
