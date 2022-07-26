115cb398b59f00bc7e9b2da51bc4ee53dbbb0b9d
lymine1996 <lymine1996@163.com>
2022-07-26 11:00:05 -0500

    Remove the package for loading overlay temporarily

--- src/App.js
+++ src/App.js
@@ -2,27 +2,13 @@ import React from "react";
 import "./App.css";
 import NavBar from "./NavBar";
 import Router from "./Router";
-import LoadingOverlay from "react-loading-overlay";
 
 function App(props) {
   return (
-    <LoadingOverlay
-      className="myLoading"
-      active={props.isActive}
-      styles={{
-        overlay: (base) => ({
-          ...base,
-          position: "fixed",
-        }),
-      }}
-      spinner
-      text="Loading..."
-    >
-      <div className="App">
-        <NavBar />
-        <Router />
-      </div>
-    </LoadingOverlay>
+    <div className="App">
+      <NavBar />
+      <Router />
+    </div>
   );
 }
 
