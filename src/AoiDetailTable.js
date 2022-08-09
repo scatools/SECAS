be948afe22e6ef9ef2015cde6c0c69db1c442245
Anthony Collini <collini.anthony@gmail.com>
2022-08-09 06:14:03 -0500

    Fix Fix view AOI score/summary button. Add future scores and graph.

--- src/AoiDetailTable.js
+++ src/AoiDetailTable.js
@@ -6,12 +6,13 @@ import { Bar } from "react-chartjs-2";
 import { calculateScore } from "./helper/aggregateHex";
 
 const AoiDetailTable = ({ activeTable, setActiveTable }) => {
-  let aoi;
-  let currentState;
+  let aoi, currentState, futureState, currentScoreObject, futureScoreObject;
   let aoiList = useSelector((state) => state.aoi);
 
   if (activeTable) {
     aoi = Object.values(aoiList).filter((aoi) => aoi.id === activeTable);
+    currentScoreObject = calculateScore(aoi, "currentHexagons");
+    futureScoreObject = calculateScore(aoi, "futureHexagons");
     currentState = {
       labels: ["Health", "Function", "Connectivity"],
       datasets: [
@@ -21,13 +22,32 @@ const AoiDetailTable = ({ activeTable, setActiveTable }) => {
           borderColor: "rgba(0,0,0,1)",
           borderWidth: 1,
           data: [
-            (calculateScore(aoi).scoreH1 +
-              calculateScore(aoi).scoreH2 +
-              calculateScore(aoi).scoreH3 +
-              calculateScore(aoi).scoreH4) /
+            (currentScoreObject.scoreH1 +
+              currentScoreObject.scoreH2 +
+              currentScoreObject.scoreH3 +
+              currentScoreObject.scoreH4) /
               4,
-            (calculateScore(aoi).scoreF1 + calculateScore(aoi).scoreF2) / 2,
-            (calculateScore(aoi).scoreC1 + calculateScore(aoi).scoreC2) / 2,
+            (currentScoreObject.scoreF1 + currentScoreObject.scoreF2) / 2,
+            (currentScoreObject.scoreC1 + currentScoreObject.scoreC2) / 2,
+          ],
+        },
+      ],
+    };
+    futureState = {
+      labels: ["Health", "Function", "Connectivity"],
+      datasets: [
+        {
+          backgroundColor: "rgba(75,192,192,1)",
+          borderColor: "rgba(0,0,0,1)",
+          borderWidth: 1,
+          data: [
+            (futureScoreObject.scoreH1 +
+              futureScoreObject.scoreH2 +
+              futureScoreObject.scoreH3 +
+              futureScoreObject.scoreH4) /
+              4,
+            (futureScoreObject.scoreF1 + futureScoreObject.scoreF2) / 2,
+            (futureScoreObject.scoreC1 + futureScoreObject.scoreC2) / 2,
           ],
         },
       ],
@@ -46,77 +66,103 @@ const AoiDetailTable = ({ activeTable, setActiveTable }) => {
       >
         X
       </div>
+
       <div className="AoiTable" style={{ padding: "20px", marginTop: "50px" }}>
+        <h2>AOI Scores</h2>
         {aoi && (
           <>
             <Table striped bordered size="sm" variant="dark">
               <thead>
                 <tr>
                   <th>Measures</th>
-                  <th>Score</th>
+                  <th>Current</th>
+                  <th>Future</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td colSpan="2">
-                    <b>Health: </b>{" "}
+                    <b>Health</b>
                   </td>
                 </tr>
                 <tr>
                   <td>Open Pine Site Condition:</td>
-                  <td>{calculateScore(aoi).scoreH1}</td>
+                  <td>{currentScoreObject.scoreH1}</td>
+                  <td>{futureScoreObject.scoreH1}</td>
                 </tr>
                 <tr>
                   <td>Open Pine Species:</td>
-                  <td>{calculateScore(aoi).scoreH2}</td>
+                  <td>{currentScoreObject.scoreH2}</td>
+                  <td>{futureScoreObject.scoreH2}</td>
                 </tr>
                 <tr>
                   <td>Toby's Fire:</td>
-                  <td>{calculateScore(aoi).scoreH3}</td>
+                  <td>{currentScoreObject.scoreH3}</td>
+                  <td>{futureScoreObject.scoreH3}</td>
                 </tr>
                 <tr>
                   <td>Conservation Management:</td>
-                  <td>{calculateScore(aoi).scoreH4}</td>
+                  <td>{currentScoreObject.scoreH4}</td>
+                  <td>{futureScoreObject.scoreH4}</td>
                 </tr>
                 <tr>
                   <td colSpan="2">
-                    <b>Function: </b>{" "}
+                    <b>Function</b>
                   </td>
                 </tr>
                 <tr>
                   <td>Forest Carbon:</td>
-                  <td>{calculateScore(aoi).scoreF1}</td>
+                  <td>{currentScoreObject.scoreF1}</td>
+                  <td>{futureScoreObject.scoreF1}</td>
                 </tr>
                 <tr>
                   <td>Working Lands:</td>
-                  <td>{calculateScore(aoi).scoreF2}</td>
+                  <td>{currentScoreObject.scoreF2}</td>
+                  <td>{futureScoreObject.scoreF2}</td>
                 </tr>
                 <tr>
                   <td colSpan="2">
-                    <b>Connectivity:</b>{" "}
+                    <b>Connectivity</b>
                   </td>
                 </tr>
                 <tr>
                   <td>Open Pine Landscape Condition: </td>
-                  <td>{calculateScore(aoi).scoreC1}</td>
+                  <td>{currentScoreObject.scoreC1}</td>
+                  <td>{futureScoreObject.scoreC1}</td>
                 </tr>
                 <tr>
                   <td>TNC Resilience:</td>
-                  <td>{calculateScore(aoi).scoreC2}</td>
+                  <td>{currentScoreObject.scoreC2}</td>
+                  <td>{futureScoreObject.scoreC2}</td>
                 </tr>
               </tbody>
             </Table>
             <Bar
               data={currentState}
               options={{
-                title: {
-                  display: "true",
-                  text: "Overall Score",
-                  fontSize: 20,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: "Current AOI Scores",
+                  },
                 },
-                legend: {
-                  display: "true",
-                  position: "right",
+              }}
+            />
+            <hr />
+            <Bar
+              data={futureState}
+              options={{
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: "Future AOI Scores",
+                  },
                 },
               }}
             />
